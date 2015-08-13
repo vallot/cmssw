ef4d518ccf0f204acfdc7c303771cb8e21efac28
Junghwan John Goh <jhgoh@cern.ch>
2015-08-13 13:11:03 +0200

    prevent null pointer exception

--- TopQuarkAnalysis/TopEventProducers/src/PseudoTopProducer.cc
+++ TopQuarkAnalysis/TopEventProducers/src/PseudoTopProducer.cc
@@ -217,6 +217,7 @@ void PseudoTopProducer::produce(edm::Event& event, const edm::EventSetup& eventS
       const size_t index = fjConstituents[j].user_index();
       if ( bHadronIdxs.find(index) != bHadronIdxs.end() ) hasBHadron = true;
       reco::CandidatePtr cand = finalStateHandle->ptrAt(index);
+      if ( cand.isNull() ) continue;
       constituents.push_back(cand);
     }
     
